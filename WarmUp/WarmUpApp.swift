245596c3457335347aa9ed5c5e692cf990f6d5c6
torinunna <torinunna9588@gmail.com>
2024-03-17 15:09:21 +0900

    Initial Commit

---
+++ WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by YUJIN KWON on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
